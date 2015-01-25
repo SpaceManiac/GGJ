611c9c1d3e1e638e7ce7b70b66dd2e2370d62ced
Tad Hardesty <tad@platymuus.com>
2015-01-24 19:30:31 -0600

    Horrified obelisk.

--- src/main/java/com/platymuus/ggj15/game/Obelisk.java
+++ src/main/java/com/platymuus/ggj15/game/Obelisk.java
@@ -2,7 +2,7 @@ package com.platymuus.ggj15.game;
 
 import com.platymuus.ggj15.Resources;
 import com.platymuus.jsc.BoundsHandler;
-import com.platymuus.jsc.Hacks;
+import com.platymuus.jsc.gui.Group;
 import org.jsfml.graphics.*;
 import org.jsfml.system.Vector2f;
 
@@ -12,13 +12,19 @@ import org.jsfml.system.Vector2f;
 public class Obelisk extends Interactable {
 
     private Sprite sprite;
+    private Sprite horror;
+    private Sprite glow;
 
     public Obelisk() {
         location = new Vector2f(0, -50);
 
         sprite = Resources.getSprite("game/obelisk.png");
+        horror = Resources.getSprite("game/horror-obelisk.png");
+        glow = Resources.getSprite("game/obelisk-glow.png");
         BoundsHandler.of(sprite).position(0.5f, 1f);
-        drawable = sprite;
+        BoundsHandler.of(horror).position(0.5f, 1f);
+        glow.setOrigin(Vector2f.add(horror.getOrigin(), new Vector2f(50, 50)));
+        drawable = new Group(glow, sprite, horror);
 
         collision = new FloatRect(-20, -20, 40, 40);
     }
@@ -45,7 +51,8 @@ public class Obelisk extends Interactable {
 
     @Override
     public void draw(RenderTarget renderTarget, RenderStates renderStates) {
-        sprite.setColor(Hacks.multiply(Color.WHITE, 0.5f + 0.5f * (float) world.landmarks / world.totalLandmarks));
+        horror.setColor(new Color(Color.WHITE, 255 * world.landmarks / world.totalLandmarks));
+        glow.setColor(new Color(Color.WHITE, world.landmarks == world.totalLandmarks ? (int)((216) + 64 * Math.sin(System.currentTimeMillis() / 700.)) : 0));
         super.draw(renderTarget, renderStates);
     }
 }
